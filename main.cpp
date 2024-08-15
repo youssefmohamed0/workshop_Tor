f73b12108bf2a924ab2c30f4d9a40466db77fb8b
youssef <youssefyoussef.m4@gmail.com>
2024-08-15 20:27:16 +0300

    Add bye function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ void greet()
     cout<<"Welcome to program!"<<endl;
 }
 
+void bye(string name)
+{
+    cout<<"Thank you "<< name<<" for using our program!"<<endl;
+}
+
 int main()
 {
     greet();
