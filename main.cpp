1c3a4055e6dae1bf9775c114d7a0da04a5ab34df
youssef <youssefyoussef.m4@gmail.com>
2024-08-15 20:25:32 +0300

    Add function for asking for name

--- main.cpp
+++ main.cpp
@@ -8,8 +8,17 @@ void greet()
     cout<<"Welcome to program!"<<endl;
 }
 
+string askForName()
+{
+    string name;
+    cout<<"Enter your name: "<<endl;
+    cin>> name;
+    return name;
+}
+
 int main()
 {
     greet();
+    string name = askForName();
     return 0;
 }
